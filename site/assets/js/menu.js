c489c8c694f81a7d1d4b2d71ff3eb92f23da9dcf
Thales Ernane de Souza <talesernani@gmail.com>
2020-06-19 12:21:53 -0300

    Atualizando Prjeto - minificando css, js

--- site/assets/js/menu.js
+++ site/assets/js/menu.js
@@ -1,18 +1,3 @@
-/* codigo com Jquery
-
-$(window).on('load', function() {
-	var menu = $('.header-menu');
-
-	$('.header-menu-titulo').on('click', function() {
-		if (menu.is('[data-ativo]')) {
-			menu.removeAttr('data-ativo');
-		} else {
-			menu.attr('data-ativo', '');
-		}
-	});
-
-});
-*/
 
 window.addEventListener('load', function() {
     var titulo = document.querySelector('.header-menu-titulo');
